0e27efa01e81cb7e619aa348be4de2d2acf472a8
Marley Abe <marleyabe@gmail.com>
2023-06-22 15:53:51 -0300

    add script

---
+++ script.js
@@ -0,0 +1,50 @@
+const textarea = document.getElementById('message');
+
+textarea.value = '';
+textarea.value += '';
+
+//header
+const header = document.getElementById('header');
+
+header.addEventListener('click', function handleClick() {
+textarea.value += '###';
+});
+
+// code
+const code = document.getElementById('code');
+
+code.addEventListener('click', function handleClick() {
+textarea.value += '``';
+});
+
+// quote
+const quote = document.getElementById('quote');
+
+quote.addEventListener('click', function handleClick() {
+textarea.value += '>';
+});
+
+// link
+const link = document.getElementById('link');
+
+link.addEventListener('click', function handleClick() {
+textarea.value += '[](url)';
+});
+
+// check
+const check = document.getElementById('check');
+
+check.addEventListener('click', function handleClick() {
+textarea.value += '\n- [X] Item 1';
+textarea.value += '\n- [   ] Item 2';
+textarea.value += '\n- [   ] Item 3';
+});
+
+// list
+const list = document.getElementById('list');
+
+list.addEventListener('click', function handleClick() {
+textarea.value += '\n- Item 1';
+textarea.value += '\n- Item 2';
+textarea.value += '\n- Item 3';
+});
